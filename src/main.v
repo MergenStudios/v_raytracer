83c414410ea669b0c2f0c1fe6c8cd660af8419e0
Mergen <paulthorand.2005@gmail.com>
2024-04-03 22:33:25 +0200

    Currently working on positionable camera, this version has moving the position of the camera working, not sure about panning

--- src/main.v
+++ src/main.v
@@ -1,7 +1,6 @@
 module main
 
 import os
-import math
 
 fn main() {
 	// parse w and h from command line
@@ -14,18 +13,16 @@ fn main() {
 	// bg_color := ColorInt{156, 196, 255}.to_float() // stole that blue from "raytracing in one weekend"
 	bg_color := ColorFloat{1.0, 1.0, 1.0} // this is ambient light
 
-	mut s := init_scene(bg_color, samples_parsed)
-
-	// translate := Vec{-1, 0, .5}
-	// t2 := make_triangle(
-	// 	Vec{-.5, -.5, 1} + translate,	// a
-	// 	Vec{.5, -.5, 1} + translate,	// b
-	// 	Vec{0, .16, 1} + translate,		// c
-	// 	Optics{
-	// 		matte_color: ColorInt{102, 0, 153}.to_float()
-	// 	}
-	// )
-	// s.add_object(t2)
+	mut s := Scene{
+		bg_color: bg_color,
+		depth: 10,
+		samples: samples_parsed,
+		cam: Camera{
+			pos: Vec{.2, 0, 0},
+			facing: Vec{0, 0, 1},
+			focal_length: 2.0
+		}
+	}
 
 	ground := make_plane(
 		Vec{0, -.5, 0}, // a (Stützvektor)
@@ -37,14 +34,32 @@ fn main() {
 	)
 	s.add_object(ground)
 
-	sphere := Sphere{
-		center: Vec{-2, 0, 17},
+	sphere_center := Sphere{
+		center: Vec{0, 0, 4},
 		radius: .5,
 		optics: Optics{
-			matte_color: ColorFloat{0.0, 1.0, 1.0}
+			matte_color: ColorFloat{0.0, 1.0, 0.0}
 		}
 	}
-	s.add_object(sphere)
+	s.add_object(sphere_center)
+
+	sphere_back := Sphere{
+		center: Vec{-.5, 0, 5.5},
+		radius: .5,
+		optics: Optics{
+			matte_color: ColorFloat{1.0, 0.0, 0.0}
+		}
+	}
+	s.add_object(sphere_back)
+
+	sphere_front := Sphere{
+		center: Vec{.5, 0, 2.5},
+		radius: .5,
+		optics: Optics{
+			matte_color: ColorFloat{0.0, 0.0, 1.0}
+		}
+	}	
+	s.add_object(sphere_front)
 
 	// triangle := make_triangle(
 	// 	Vec{-2, -.5, 21},
@@ -58,7 +73,7 @@ fn main() {
 	
 	// light source
 	my_light_source := LightSource{
-		pos: Vec{-1, 0, 17}
+		pos: Vec{0, 0, 0}
 	}
 	s.add_light_source(my_light_source)
 	
--- src/scene.v
+++ src/scene.v
@@ -2,18 +2,172 @@ module main
 
 import os
 import math
-import rand
 
 struct Camera {
 	mut:
-		pos Vec					// usually 0|0|0
-		focal_length f64		// usually 1.0
-		viewport_height f64		// usually 2.0
-		viewport_width f64		// calculated from height based on img w and h
-		pixel_delta_v f64		// calculated from viewport height
-		pixel_delta_h f64		// calculated from viewport width
-		top_left_pixel Vec		// calculated from all the above
+		pos Vec								// usually 0|0|0
+		facing Vec
+
+		focal_length f64 = 1.0				// usually 1.0
+		viewport_height f64	= 2.0			// usually 2.0 (idk why but its from rt in one weekend)
+		viewport_width f64					// calculated from height based on img w and h
+
+		up Vec = Vec{0, 1, 0} // this points up. changing this tilts the camera
+
+		pixel_delta_v Vec					// calculated from viewport height //! change this to a vector
+		pixel_delta_h Vec					// calculated from viewport width //! change this to a vector
+		top_left_pixel Vec					// calculated from all the above
 }
+
+fn (mut c Camera) setup(w int, h int) {	
+	// calculate Viewport width
+	// ratio between w and h
+	aspect_ratio := f64(w) / f64(h)
+	c.viewport_width = c.viewport_height * aspect_ratio
+
+	println("viewport_width: ${c.viewport_width}")
+	println("viewport_height: ${c.viewport_height}")
+
+
+	// ********* assuming pos 0, 0, 0
+	// viewport_width is viewport_height multiplied by aspect_ratio
+
+	pixel_delta_v_value := c.viewport_height / f64(h) // pixel_delta_v (vertical) is viewport_height scaled by 1/h
+	pixel_delta_h_value := c.viewport_width / f64(w) // pixel_delta_h (horizontal) is viewport_width scaled by 1/w
+
+	// pixel_delta_v := Vec{0, -pixel_delta_v_value, 0}
+	// pixel_delta_h := Vec{pixel_delta_h_value, 0, 0}
+
+	// on_z := c.pos + Vec{0, 0, c.focal_length}
+	// top_left_corner := on_z - Vec{c.viewport_width, 0, 0}.scale(.5) + Vec{0, c.viewport_height, 0}.scale(.5)
+	// top_left_pixel := top_left_corner + Vec{pixel_delta_h_value, 0, 0}.scale(.5) + Vec{0, -pixel_delta_v_value, 0}.scale(.5)
+
+	// ********* custom pos
+
+	// relative coordinate frame for the camera
+	relative_z := c.facing.unit()
+	relative_x := c.up.cross(relative_z).unit()
+	relative_y := relative_z.cross(relative_x).unit()
+
+	println("relative_z: ${relative_z}, len: ${relative_z.length()}")
+	println("relative_x: ${relative_x}, len: ${relative_x.length()}")
+	println("relative_y: ${relative_y}, len: ${relative_y.length()}")
+	println("\n\n")
+
+	// calculate the pixel_delta_v and pixel_delta_h
+	pixel_delta_v_ := relative_y.scale(-pixel_delta_v_value) // negative because we want the vector to go down
+	pixel_delta_h_ := relative_x.scale(pixel_delta_h_value)
+
+	println("pixel_delta_v_: ${pixel_delta_v_}, len: ${pixel_delta_v_.length()}")
+	println("pixel_delta_h_: ${pixel_delta_h_}, len: ${pixel_delta_h_.length()}")
+	println("\n\n")
+
+
+	// calculate the viewport center
+	viewport_center_ := c.pos + relative_z.scale(c.focal_length)
+	top_left_corner_ := viewport_center_ - relative_x.scale(c.viewport_width / 2) + relative_y.scale(c.viewport_height / 2)
+	top_left_pixel_ := top_left_corner_ + pixel_delta_v_.scale(.5) + pixel_delta_h_.scale(.5)
+
+	println("viewport_center_: ${viewport_center_}")
+	println("top_left_corner_: ${top_left_corner_}")
+	println("top_left_pixel_: ${top_left_pixel_}")
+	println("\n\n")
+
+
+	use_custom_pos := true
+
+	if use_custom_pos {
+		c = Camera{
+			pos: c.pos,
+			focal_length: c.focal_length,
+			viewport_height: c.viewport_height,
+			viewport_width: c.viewport_width,
+			pixel_delta_v: pixel_delta_v_,
+			pixel_delta_h: pixel_delta_h_,
+			top_left_pixel: top_left_pixel_
+		}
+	} else {
+		// c = Camera{
+		// 	pos: c.pos,
+		// 	focal_length: c.focal_length,
+		// 	viewport_height: c.viewport_height,
+		// 	viewport_width: c.viewport_width,
+		// 	pixel_delta_v: pixel_delta_v,
+		// 	pixel_delta_h: pixel_delta_h,
+		// 	top_left_pixel: top_left_pixel
+		// }
+	}
+
+
+	/*
+
+	pos := Vec{0, 0, 0}
+	
+	// calculate Viewport width
+	// ratio between w and h
+	aspect_ratio := f64(w) / f64(h)
+	focal_length := 2.7 // todo: make this a parameter later
+	// viewport_width is viewport_height multiplied by aspect_ratio
+	viewport_height := 2.0
+	viewport_width := viewport_height * aspect_ratio
+
+	pixel_delta_v_value := viewport_height / f64(h) // pixel_delta_v (vertical) is viewport_height scaled by 1/h
+	pixel_delta_h_value := viewport_width / f64(w) // pixel_delta_h (horizontal) is viewport_width scaled by 1/w
+
+	pixel_delta_v := Vec{0, -pixel_delta_v_value, 0}
+	pixel_delta_h := Vec{pixel_delta_h_value, 0, 0}
+
+	println("pixel_delta_v: ${pixel_delta_v}, len: ${pixel_delta_v.length()}")
+	println("pixel_delta_h: ${pixel_delta_h}, len: ${pixel_delta_h.length()}")
+
+
+	on_z := pos + Vec{0, 0, focal_length}
+	top_left_corner := on_z - Vec{viewport_width, 0, 0}.scale(.5) + Vec{0, viewport_height, 0}.scale(.5)
+	top_left_pixel := top_left_corner + pixel_delta_h.scale(.5) + pixel_delta_v.scale(.5)
+
+	println("on_z: ${on_z}")
+	println("top_left_corner: ${top_left_corner}")
+	println("top_left_pixel: ${top_left_pixel}")
+
+
+	// calculate the relative basis vectors for the camera
+	// todo: make sure that the coordinate system doesnt get flipped somehow
+	relative_z := direction.unit()
+	relative_x := up.cross(relative_z).unit()
+	relative_y := relative_z.cross(relative_x).unit()
+
+	println("relative_z: ${relative_z}, len: ${relative_z.length()}")
+	println("relative_x: ${relative_x}, len: ${relative_x.length()}")
+	println("relative_y: ${relative_y}, len: ${relative_y.length()}")
+
+	// calculate the pixel_delta_v and pixel_delta_h
+	pixel_delta_v_ := relative_y.scale(-pixel_delta_v_value) // negative because we want the vector to go down
+	pixel_delta_h_ := relative_x.scale(pixel_delta_h_value)
+	
+	println("pixel_delta_v_: ${pixel_delta_v_}, len: ${pixel_delta_v_.length()}")
+	println("pixel_delta_h_: ${pixel_delta_h_}, len: ${pixel_delta_h_.length()}")
+
+	// calculate the viewport center
+	viewport_center_ := pos_ + relative_z.scale(focal_length)
+	top_left_corner_ := viewport_center_ - relative_x.scale(viewport_height / 2) + relative_y.scale(viewport_width / 2)
+	top_left_pixel_ := top_left_corner_ + pixel_delta_v.scale(.5) + pixel_delta_h_.scale(.5)
+
+	println("viewport_center_: ${viewport_center_}")
+	println("top_left_corner_: ${top_left_corner_}")
+	println("top_left_pixel_: ${top_left_pixel_}")
+
+	return Camera{
+		pos: pos,
+		focal_length: focal_length,
+		viewport_height: viewport_height,
+		viewport_width: viewport_width,
+		pixel_delta_v: pixel_delta_v_,
+		pixel_delta_h: pixel_delta_h_,
+		top_left_pixel: top_left_pixel_
+	}
+	*/
+}
+
 
 struct Scene {
 	bg_color ColorFloat
@@ -25,24 +179,6 @@ struct Scene {
 		light_sources []LightSource
 }
 
-
-fn init_scene(bg_color ColorFloat, samples int) Scene {
-	// sane defaults
-	cam := Camera{
-		pos: Vec{0, 0, 0},
-		focal_length: 1.0,
-		viewport_height: 2.0
-	}
-	
-	return Scene {
-		bg_color: bg_color
-		depth: 5
-		samples: samples
-		cam: cam
-		hittable_objects: []HittableObject{}
-	}
-}
-
 fn (mut s Scene) add_object(obj HittableObject) {
 	s.hittable_objects << obj
 }
@@ -51,46 +187,14 @@ fn (mut s Scene) add_light_source(source LightSource) {
 	s.light_sources << source
 }
 
-fn setup_cam(w int, h int) Camera {
-	pos := Vec{0, 0, 0}
-	
-	// calculate Viewport width
-	// ratio between w and h
-	aspect_ratio := f64(w) / f64(h)
 
-	focal_length := 10.0
-
-	// viewport_width is viewport_height multiplied by aspect_ratio
-	viewport_height := 2.0
-	viewport_width := viewport_height * aspect_ratio
-
-	pixel_delta_v := viewport_height / f64(h) // pixel_delta_v (vertical) is viewport_height scaled by 1/h
-	pixel_delta_h := viewport_width / f64(w) // pixel_delta_h (horizontal) is viewport_width scaled by 1/w
-
-	on_z := pos + Vec{0, 0, focal_length}
-	top_left_corner := on_z - Vec{viewport_width, 0, 0}.scale(.5) + Vec{0, viewport_height, 0}.scale(.5)
-	top_left_pixel := top_left_corner + Vec{pixel_delta_h, 0, 0}.scale(.5) + Vec{0, -pixel_delta_v, 0}.scale(.5)
-	
-
-	return Camera{
-		pos: pos,
-		focal_length: focal_length,
-		viewport_height: viewport_height,
-		viewport_width: viewport_width,
-		pixel_delta_v: pixel_delta_v,
-		pixel_delta_h: pixel_delta_h,
-		top_left_pixel: top_left_pixel
-	}
-}
-
-
-fn (s Scene) get_ray(x, y int) Ray {
-	pixel_center := s.cam.top_left_pixel + Vec{s.cam.pixel_delta_h, 0, 0}.scale(x) + Vec{0, -s.cam.pixel_delta_v, 0}.scale(y)
+fn (s Scene) get_ray(x int, y int) Ray {
+	pixel_center := s.cam.top_left_pixel + s.cam.pixel_delta_h.scale(x) + s.cam.pixel_delta_v.scale(y)
 
 	py := -.5 + rand_f64(0, 1)
 	px := -.5 + rand_f64(0, 1)
 
-	pixel_sample := pixel_center + Vec{s.cam.pixel_delta_h, 0, 0}.scale(px) + Vec{0, -s.cam.pixel_delta_v, 0}.scale(py)
+	pixel_sample := pixel_center + s.cam.pixel_delta_h.scale(px) + s.cam.pixel_delta_v.scale(py)
 
 	r := Ray{s.cam.pos, pixel_sample.unit()} // todo: does this HAVE to be a unit vector?
 
@@ -159,8 +263,9 @@ fn (s Scene) trace_ray(r Ray, depth int) ColorFloat {
 	}
 }
 
+
 fn (mut s Scene) render(path string, w int, h int) !{
-	s.cam = setup_cam(w, h)
+	s.cam.setup(w, h)
 	
 	mut f := os.open_file(path, "w")!
 	
